56365dae608a8d096b892afa62eee2ccc92c3f77
Vladislav <vlad.lemeshok@gmail.com>
2024-08-31 19:06:45 +0300

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Salesforce DX Project: information
+
+![Иллюстрация к проекту](https://res.cloudinary.com/hy4kyit2a/f_auto,fl_lossy,q_70/learn/modules/lightning-web-components-basics/handle-events-in-lightning-web-components/images/cb117f7a984e3d46bb61d92e15805192_kix.54qhktfi7471.png)
---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class BikeCard extends LightningElement {
+    name = 'Atom ION';
+    desc = 'Brand new, bright bike.';
+    category = 'street';
+    material = 'steel';
+    price = '2,700$';
+    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+    
+}
